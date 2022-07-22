9afb525794e6510ab29f1503510a7a7285f6a391
DevExpressExampleBot <fakeEmail>
2022-07-22 15:25:42 +0400

    Source auto update [skip ci]

--- VB/WebDashboard_ManualDashboardState/Default.aspx.vb
+++ VB/WebDashboard_ManualDashboardState/Default.aspx.vb
@@ -1,39 +1,32 @@
-﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon
 Imports DevExpress.DashboardWeb
 Imports System
 Imports System.Collections.Generic
 
 Namespace WebDashboard_ManualDashboardState
-	Partial Public Class [Default]
-		Inherits System.Web.UI.Page
 
-		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-		End Sub
+    Public Partial Class [Default]
+        Inherits Web.UI.Page
 
-		Protected Sub ASPxDashboard1_SetInitialDashboardState(ByVal sender As Object, ByVal e As SetInitialDashboardStateEventArgs)
-			e.InitialState = InitializeDashboardState()
-		End Sub
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        End Sub
 
-		Public Function InitializeDashboardState() As DashboardState
-			Dim dashboardState As New DashboardState()
+        Protected Sub ASPxDashboard1_SetInitialDashboardState(ByVal sender As Object, ByVal e As SetInitialDashboardStateEventArgs)
+            e.InitialState = InitializeDashboardState()
+        End Sub
 
-			Dim parameterState As New DashboardParameterState("countryParameter", "USA", GetType(String))
-
-			Dim gridFilterState As New DashboardItemState("gridDashboardItem1")
-			gridFilterState.MasterFilterValues.AddRange(New List(Of Object())() From {
-				New String(0) { "Andrew Fuller" },
-				New String(0) { "Laura Callahan" }
-			})
-
-			Dim treemapDrilldownState As New DashboardItemState("treemapDashboardItem1")
-			treemapDrilldownState.DrillDownValues.Add("Beverages")
-
-			Dim rangeFilterState As New DashboardItemState("rangeFilterDashboardItem1")
-			rangeFilterState.RangeFilterState.Selection = New RangeFilterSelection(New DateTime(2015, 1, 1), New DateTime(2016, 1, 1))
-
-			dashboardState.Parameters.Add(parameterState)
-			dashboardState.Items.AddRange(New List(Of DashboardItemState)() From {gridFilterState, treemapDrilldownState, rangeFilterState})
-			Return dashboardState
-		End Function
-	End Class
+        Public Function InitializeDashboardState() As DashboardState
+            Dim dashboardState As DashboardState = New DashboardState()
+            Dim parameterState As DashboardParameterState = New DashboardParameterState("countryParameter", "USA", GetType(String))
+            Dim gridFilterState As DashboardItemState = New DashboardItemState("gridDashboardItem1")
+            gridFilterState.MasterFilterValues.AddRange(New List(Of Object())() From {New String(0) {"Andrew Fuller"}, New String(0) {"Laura Callahan"}})
+            Dim treemapDrilldownState As DashboardItemState = New DashboardItemState("treemapDashboardItem1")
+            treemapDrilldownState.DrillDownValues.Add("Beverages")
+            Dim rangeFilterState As DashboardItemState = New DashboardItemState("rangeFilterDashboardItem1")
+            rangeFilterState.RangeFilterState.Selection = New RangeFilterSelection(New DateTime(2015, 1, 1), New DateTime(2016, 1, 1))
+            dashboardState.Parameters.Add(parameterState)
+            dashboardState.Items.AddRange(New List(Of DashboardItemState)() From {gridFilterState, treemapDrilldownState, rangeFilterState})
+            Return dashboardState
+        End Function
+    End Class
 End Namespace
--- VB/WebDashboard_ManualDashboardState/Global.asax
+++ VB/WebDashboard_ManualDashboardState/Global.asax
@@ -1,1 +1,1 @@
-<%@ Application Codebehind="Global.asax.cs" Inherits="WebDashboard_DashboardState.Global_asax" Language="C#" %>
+<%@ Application Codebehind="Global.asax.vb" Inherits="WebDashboard_DashboardState.Global_asax" Language="VB" %>
